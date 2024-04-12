78e80e60144dce157b37c226b59374f301ad61f2
LeoAaswin <139626823+LeoAaswin@users.noreply.github.com>
2024-04-12 13:59:19 +0545

    Building an won api using webscraping and requests

--- api/currency_exg.py
+++ api/currency_exg.py
@@ -1,12 +1,13 @@
 
-from flask import Flask
+from flask import Flask, jsonify
 
 from bs4 import BeautifulSoup
 import requests
 
 def get_currency(in_currency, out_currency):
-    url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'\
-    content = requests.get(url)
+    url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+    content = requests.get(url).text
+
     soup = BeautifulSoup(content, 'html.parser')
     rate = soup.find("span", class_="ccOutputRslt").get_text()
     rate = float(rate[:-4])
