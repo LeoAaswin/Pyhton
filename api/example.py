1c6c546e46371155cd425766f3d1acbe19ba7fde
LeoAaswin <139626823+LeoAaswin@users.noreply.github.com>
2024-04-12 13:04:28 +0545

    Buiding an api

--- api/example.py
+++ api/example.py
@@ -1,1 +1,12 @@
-hello world
+import requests
+
+def weather_forecast(city,key='26631f0f41b95fb9f5ac0df9a8f43c92',units='metric'):
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={key}&units={units}'
+    x = requests.get(url)
+    forecast = x.json()
+    return forecast
+
+
+
+print(weather_forecast(city='kathmandu'))
+
